d264e43afff60a4bafec830409a8d64f2ab3f635
Adhiel Asiabel <vincent.adhiel28@gmail.com>
2023-11-14 08:33:29 +0700

    timsar

--- app/src/main/java/com/example/tolong_aku/Tim_sar.kt
+++ app/src/main/java/com/example/tolong_aku/Tim_sar.kt
@@ -7,8 +7,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.core.content.ContextCompat.startActivities
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class Tim_sar : AppCompatActivity(), View.OnClickListener {
+class Tim_sar : AppCompatActivity(), View.OnClickListener, OnMapReadyCallback{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tim_sar)
