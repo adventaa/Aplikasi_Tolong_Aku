9bfab55113643018b0ba2f162ea0b60f9fe6edd8
Adhiel Asiabel <vincent.adhiel28@gmail.com>
2023-11-23 08:05:46 +0700

    benerin layout awal rumah sakit(2)

--- app/src/main/java/com/example/tolong_aku/Rumah_sakit.kt
+++ app/src/main/java/com/example/tolong_aku/Rumah_sakit.kt
@@ -1,21 +1,20 @@
 package com.example.tolong_aku
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 
 class Rumah_sakit : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rumah_sakit)
 
-        val buttonClick: Button = findViewById(R.id.button_dokter)
+        val buttonClick: ImageButton = findViewById(R.id.buttonDokter)
         buttonClick.setOnClickListener(this)
 
-        val buttonClick2: Button = findViewById(R.id.button_ambulance)
+        val buttonClick2: ImageButton = findViewById(R.id.buttonAmbulance)
         buttonClick2.setOnClickListener(this)
 
         val buttonClick3: ImageButton = findViewById(R.id.imageButton)
@@ -29,11 +28,11 @@ class Rumah_sakit : AppCompatActivity(), View.OnClickListener {
                     val moveIntent = Intent(this, MainMenu::class.java)
                     startActivities(arrayOf(moveIntent))
                 }
-                R.id.button_dokter -> {
+                R.id.buttonDokter -> {
                     val moveIntent = Intent(this, Dokter::class.java)
                     startActivities(arrayOf(moveIntent))
                 }
-                R.id.button_ambulance -> {
+                R.id.buttonAmbulance -> {
                     val moveIntent = Intent(this, Ambulance::class.java)
                     startActivities(arrayOf(moveIntent))
                 }
