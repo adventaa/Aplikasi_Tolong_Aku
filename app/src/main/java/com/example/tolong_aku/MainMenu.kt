f72c03c4fff18a7ae2f92ca4f77493d86a1b595b
Adhiel Asiabel <vincent.adhiel28@gmail.com>
2023-11-23 21:28:00 +0700

    benerin main menu (2) + ujicoba

--- app/src/main/java/com/example/tolong_aku/MainMenu.kt
+++ app/src/main/java/com/example/tolong_aku/MainMenu.kt
@@ -1,13 +1,30 @@
 package com.example.tolong_aku
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
-class MainMenu : AppCompatActivity() {
+class MainMenu : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
+        val buttonClick : ImageButton = findViewById(R.id.buttonKeRumahSakit)
+        buttonClick.setOnClickListener(this)
+
     }
 
+    override fun onClick(p0: View?) {
+        if (p0 != null) {
+            when (p0.id) {
+                R.id.buttonKeRumahSakit -> {
+                    val moveIntent = Intent(this, Rumah_sakit::class.java)
+                    startActivities(arrayOf(moveIntent))
+                }
+
+            }
+        }
+    }
 }
