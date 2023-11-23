12588680d656f5b211d3ffef638f49bfc204d30e
Adhiel Asiabel <vincent.adhiel28@gmail.com>
2023-11-23 20:20:51 +0700

    benerin rumah sakit lagi

--- app/src/main/java/com/example/tolong_aku/Rumah_sakit.kt
+++ app/src/main/java/com/example/tolong_aku/Rumah_sakit.kt
@@ -3,6 +3,7 @@ package com.example.tolong_aku
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,14 +18,14 @@ class Rumah_sakit : AppCompatActivity(), View.OnClickListener {
         val buttonClick2: ImageButton = findViewById(R.id.buttonAmbulance)
         buttonClick2.setOnClickListener(this)
 
-        val buttonClick3: ImageButton = findViewById(R.id.imageButton)
+        val buttonClick3: Button = findViewById(R.id.buttonback)
         buttonClick3.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         if (v != null) {
             when (v.id) {
-                R.id.imageButton -> {
+                R.id.buttonback -> {
                     val moveIntent = Intent(this, MainMenu::class.java)
                     startActivities(arrayOf(moveIntent))
                 }
