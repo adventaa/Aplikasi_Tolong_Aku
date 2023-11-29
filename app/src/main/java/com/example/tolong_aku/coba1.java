72c694097aa2f97d6bbc999280b9a53c22aba870
Adhiel Asiabel <vincent.adhiel28@gmail.com>
2023-11-29 20:20:33 +0700

    benerin layout lanjutan masing2 penyedia layanan

---
+++ app/src/main/java/com/example/tolong_aku/coba1.java
@@ -0,0 +1,14 @@
+package com.example.tolong_aku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class coba1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coba1);
+    }
+}
