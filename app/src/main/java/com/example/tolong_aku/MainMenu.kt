536a3e9798896b0662082e1d1e6c8d5d80e970d4
Adhiel Asiabel <vincent.adhiel28@gmail.com>
2023-12-01 11:57:44 +0700

    layout home (2)

--- app/src/main/java/com/example/tolong_aku/MainMenu.kt
+++ app/src/main/java/com/example/tolong_aku/MainMenu.kt
@@ -2,12 +2,30 @@ package com.example.tolong_aku
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.tolong_aku.databinding.ActivityMainMenuBinding
 
 class MainMenu : AppCompatActivity() {
+    private lateinit var binding : ActivityMainMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_menu)
 
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+            }
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 
 }
