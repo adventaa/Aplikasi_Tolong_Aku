62ded71c4d3ad4d0965f1fe3e4b3b43db4fa3357
Adhiel Asiabel <vincent.adhiel28@gmail.com>
2023-12-16 14:38:53 +0700

    maps sementara (belum dicoba)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    sourceSets {
+        getByName("main") {
+            assets {
+                srcDirs("src/main/assets")
+            }
+        }
+    }
     buildFeatures{
         viewBinding = true
     }
@@ -51,6 +58,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("org.osmdroid:osmdroid-android:6.1.10")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -88,4 +96,6 @@ dependencies {
     ksp ("com.github.bumptech.glide:compiler:4.12.0")
 
     implementation ("androidx.navigation:navigation-compose:2.5.1")
+
+    implementation ("org.osmdroid:osmdroid-android:6.1.11")
 }
---
+++ app/src/main/java/com/example/tolong_aku/ModelMain.kt
@@ -0,0 +1,9 @@
+package com.example.tolong_aku
+
+class ModelMain {
+
+    lateinit var strName : String
+    lateinit var strVicinity : String
+//    var latloc : 0.0
+//    var longloc : 0.0
+}
