3cfe777e039e04fe52275ee437bc81a0bdef66db
Adhiel Asiabel <vincent.adhiel28@gmail.com>
2023-12-17 09:03:06 +0700

    maps (final)

--- app/src/main/java/com/example/tolong_aku/Ambulance_lanjutan.kt
+++ app/src/main/java/com/example/tolong_aku/Ambulance_lanjutan.kt
@@ -1,13 +1,45 @@
 package com.example.tolong_aku
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONException
+import org.json.JSONObject
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapController
+import org.osmdroid.views.overlay.OverlayItem
+import java.io.IOException
+import java.nio.charset.StandardCharsets
 
 class Ambulance_lanjutan : AppCompatActivity() , View.OnClickListener {
+
+    var modelMainList: MutableList<ModelMain> = ArrayList()
+    lateinit var mapController: MapController
+    lateinit var overlayItem: ArrayList<OverlayItem>
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
+        val geoPoint = GeoPoint(-6.3035467, 106.8693513)
+        val mapView: org.osmdroid.views.MapView = findViewById(R.id.mapView)
+        mapView.setMultiTouchControls(true)
+        mapView.controller.animateTo(geoPoint)
+        mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+        mapView.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
+
+        mapController = mapView.controller as MapController
+        mapController.setCenter(geoPoint)
+        mapController.zoomTo(15)
+
+        getLocationMarker()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ambulance_lanjutan)
 
@@ -23,6 +55,43 @@ class Ambulance_lanjutan : AppCompatActivity() , View.OnClickListener {
                     startActivities(arrayOf(moveIntent))
                 }
             }
+        }
+    }
+
+    private fun getLocationMarker() {
+        try {
+            val stream = assets.open("sample_maps.json")
+            val size = stream.available()
+            val buffer = ByteArray(size)
+            stream.read(buffer)
+            stream.close()
+            val strContent = String(buffer, StandardCharsets.UTF_8)
+            try {
+                val jsonObject = JSONObject(strContent)
+                val jsonArrayResult = jsonObject.getJSONArray("results")
+                for (i in 0 until jsonArrayResult.length()) {
+                    val jsonObjectResult = jsonArrayResult.getJSONObject(i)
+                    val modelMain = ModelMain()
+                    modelMain.strName = jsonObjectResult.getString("name")
+                    modelMain.strVicinity = jsonObjectResult.getString("vicinity")
+
+                    //get lat long
+                    val jsonObjectGeo = jsonObjectResult.getJSONObject("geometry")
+                    val jsonObjectLoc = jsonObjectGeo.getJSONObject("location")
+//                    modelMain.latLoc = jsonObjectLoc.getDouble("lat")
+//                    modelMain.longLoc = jsonObjectLoc.getDouble("lng")
+                    modelMainList.add(modelMain)
+                }
+//                initMarker(modelMainList)
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        } catch (ignored: IOException) {
+            Toast.makeText(
+                this@Ambulance_lanjutan,
+                "Oops, ada yang tidak beres. Coba ulangi beberapa saat lagi.",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
