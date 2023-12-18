4df3750c1e93fc87ee522d30da7b5a5e51263075
sebastianCodenew <mariasitiwuryaningsih957@gmail.com>
2023-12-18 11:16:30 +0700

    edit hp baru

--- app/src/main/java/com/example/tolong_aku/LoginHelper.kt
+++ app/src/main/java/com/example/tolong_aku/LoginHelper.kt
@@ -12,11 +12,9 @@ class LoginHelper : AppCompatActivity() {
     private lateinit var binding: ActivityLoginHelperBinding
     private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    super.onCreate(savedInstanceState)
         binding = ActivityLoginHelperBinding.inflate(layoutInflater)
-
-//        setContentView(R.layout.activity_login_helper)
-          setContentView(binding.root)
+        setContentView(binding.root)
 
 
         firebaseAuth = FirebaseAuth.getInstance()
