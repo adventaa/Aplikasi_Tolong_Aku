d7bf1fc6560f43ca2b1ff216d400428c0d502f09
sebastianCodenew <mariasitiwuryaningsih957@gmail.com>
2023-12-18 23:03:18 +0700

    edit hp baru

--- app/src/main/java/com/example/tolong_aku/LoginHelper.kt
+++ app/src/main/java/com/example/tolong_aku/LoginHelper.kt
@@ -14,6 +14,7 @@ class LoginHelper : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
         binding = ActivityLoginHelperBinding.inflate(layoutInflater)
+        //
         setContentView(binding.root)
 
 
